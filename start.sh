df928fa3360ce0a0bc2a1789d41f872c5f1c8b5a
mindamp <mindamp@gmail.com>
2019-01-02 18:57:50 +0000

    Bleh... automation and stuff.

--- start.sh
+++ start.sh
@@ -2,10 +2,10 @@
 cd "$(dirname "$0")"
 CURRENT_VERSION="9.0.0"
 #CURRENT_VERSION="$(curl -s 'https://raw.githubusercontent.com/worldautomation/WA-Launcher-Pack/master/app/assets/distribution.json' | awk '/version/{i++}i==2{print; exit}' | awk -F "\"*:\"*" '{print $2}' | cut -c 3- | cut -c -7)"
-if ! screen -list | grep -q "mc"; then
+if ! screen -list | grep -q "resist"; then
 	echo "Server is starting!"
 	sed s/WA_VERSION/$CURRENT_VERSION/g server.properties.template > server.properties
-	sudo screen -dmS mc sudo java -server -XX:+UseConcMarkSweepGC -XX:+UseParNewGC -XX:+CMSIncrementalPacing -XX:+AggressiveOpts -Xms1G -Xmx10G -jar Thermos-1.7.10-1614-server.jar nogui
+	sudo screen -dmS resist sudo java -server -XX:+UseConcMarkSweepGC -XX:+UseParNewGC -XX:+CMSIncrementalPacing -XX:+AggressiveOpts -Xms1G -Xmx10G -jar Thermos-1.7.10-1614-server.jar nogui
 else
 	echo "Server already started!";
 fi
